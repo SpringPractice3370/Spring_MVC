ba5067760289c55c01ed341ceee143a0bfe32112
sectionr0 <sectionr0@gmail.com>
2022-07-14 23:15:15 +0900

    data-jpa
    data-jpa

---
+++ data-jpa/src/main/java/com/example/datajpa/repository/MemberRepository.java
@@ -0,0 +1,24 @@
+package com.example.datajpa.repository;
+
+import com.example.datajpa.dto.MemberDto;
+import com.example.datajpa.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select new com.example.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+}
